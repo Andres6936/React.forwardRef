b11a1f501329ef67d7651bc5e0a96eebfd81cc5a
Andres6936 <andres6936@gmail.com>
2023-06-17 20:47:15 -0500

    Change of Style

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ function App() {
                 <div className="flex flex:col mt:1em">
                     <label className={"color:white@dark mb:0.2em"}>Message</label>
                     <input
-                        className={"b:1px|solid|#CCC b:1px|solid|black@dark r:1.5em px:1em py:0.5em bg:gray-50@dark"}
+                        className={"b:1px|solid|#CCC b:1px|solid|black@dark r:1.5em px:1em py:0.5em color:white@dark bg:gray-50@dark"}
                         value={message}
                         onChange={({target}) => setMessage(target.value)}
                     />
@@ -86,7 +86,7 @@ function App() {
                 <div className="flex flex:col">
                     <label className={"color:white@dark mb:0.2em"}>Log History</label>
                     <textarea
-                        className={"b:1px|solid|#CCC b:1px|solid|black@dark r:1em bg:gray-50@dark"}
+                        className={"b:1px|solid|#CCC b:1px|solid|black@dark color:white@dark px:1em py:0.5em r:1em bg:gray-50@dark"}
                         rows={5}
                         value={formatHistory()}
                         readOnly={true}/>
